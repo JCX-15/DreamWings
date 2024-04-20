34c137f1f40cfc8a2243e510235527643622c611
AlejoZeron <ale25.zeron@gmail.com>
2024-04-20 11:36:20 -0600

    no se que es esto pero ahi te lo mando

--- src/main/java/com/dreamwings/demo/Services/AsientosServices.java
+++ src/main/java/com/dreamwings/demo/Services/AsientosServices.java
@@ -1,5 +1,16 @@
 package com.dreamwings.demo.Services;
 
+import java.util.List;
+
+import com.dreamwings.demo.Entities.Asientos;
+
 public interface AsientosServices {
     
+    public List<Asientos> asientosDeVuelo(String id);
+
+    public Asientos seleccionarAsiento(String nombreAsiento, String idVuelo);
+
+    public Asientos deseleccionarAsiento(String nombreAsiento, String idVuelo); 
+
+    public boolean estadoAsiento(String nombreAsiento, String idVuelo);
 }
--- src/main/java/com/dreamwings/demo/Services/Impl/AsientosServicesImpl.java
+++ src/main/java/com/dreamwings/demo/Services/Impl/AsientosServicesImpl.java
@@ -1,10 +1,37 @@
 package com.dreamwings.demo.Services.Impl;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.dreamwings.demo.Entities.Asientos;
 import com.dreamwings.demo.Services.AsientosServices;
 
 @Service
 public class AsientosServicesImpl implements AsientosServices{
+
+    @Override
+    public List<Asientos> asientosDeVuelo(String id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'asientosDeVuelo'");
+    }
+
+    @Override
+    public Asientos seleccionarAsiento(String nombreAsiento, String VueloId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'seleccionarAsiento'");
+    }
+
+    @Override
+    public Asientos deseleccionarAsiento(String nombreAsiento, String VueloId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deseleccionarAsiento'");
+    }
+
+    @Override
+    public boolean estadoAsiento(String nombreAsiento, String VueloId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'estadoAsiento'");
+    }
     
 }
