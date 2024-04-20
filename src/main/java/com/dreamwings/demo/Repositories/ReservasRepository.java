bc9cf1fd4757baabab6d98076089ca660eee51c1
AlejoZeron <ale25.zeron@gmail.com>
2024-04-20 10:42:31 -0600

    query de reservas

--- src/main/java/com/dreamwings/demo/Repositories/ReservasRepository.java
+++ src/main/java/com/dreamwings/demo/Repositories/ReservasRepository.java
@@ -8,6 +8,9 @@ import com.dreamwings.demo.Entities.Reservas;
 
 @Repository
 public interface ReservasRepository extends CrudRepository <Reservas, String>{
+    @Query(value = "SELECT destinos, COUNT(destinos) AS repeticiones FROM reservas GROUP BY destinos ORDER BY repeticiones DESC LIMIT 1", nativeQuery = true)
+    Reservas findMostFrequentDestino();
+
     @Query(value = "SELECT fechareserva, COUNT(fechareserva) AS repeticiones FROM reservas GROUP BY fechareserva ORDER BY repeticiones DESC LIMIT 1", nativeQuery = true)
     Reservas findMostFrequentDate();
 }
--- src/main/java/com/dreamwings/demo/Repositories/RutasRepository.java
+++ src/main/java/com/dreamwings/demo/Repositories/RutasRepository.java
@@ -1,6 +1,5 @@
 package com.dreamwings.demo.Repositories;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +7,5 @@ import com.dreamwings.demo.Entities.Rutas;
 
 @Repository
 public interface RutasRepository extends CrudRepository <Rutas,Integer> {
-    @Query(value = "SELECT destinos, COUNT(destinos) AS repeticiones FROM rutas GROUP BY destinos ORDER BY repeticiones DESC LIMIT 1", nativeQuery = true)
-    Rutas findMostFrequentDestino();
+    
 }
